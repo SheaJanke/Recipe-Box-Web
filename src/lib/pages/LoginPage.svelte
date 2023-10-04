16a2e96244265ecf3c69747d74cadce1fc47f911
Shea Janke <sheajanke@gmail.com>
2023-10-03 22:05:55 -0700

    Change images to relative paths.

--- src/lib/pages/LoginPage.svelte
+++ src/lib/pages/LoginPage.svelte
@@ -29,7 +29,7 @@
 	<div
 		class="w-0 lg:w-3/5 h-full flex items-center justify-center bg-gradient-to-tr from-blue-500 to-cyan-500"
 	>
-		<img src={`${base}/images/transparent_icon.png`} />
+		<img src="./images/transparent_icon.png" />
 	</div>
 	<div class="w-full lg:w-2/5 h-full flex items-center justify-center bg-slate-100 text-stone-950">
 		<div class="flex flex-col w-4/5 lg:w-3/5">
@@ -59,7 +59,7 @@
 			>
 			<div class="w-full text-center mb-2">Or</div>
 			<button type="button" class="btn border border-blue-500" on:click={signInWIthGoogle}>
-				<img class="w-6 h-6" src={`${base}/images/google_icon.png`} />
+				<img class="w-6 h-6" src="./images/google_icon.png" />
 				<span>Log In With Google</span>
 			</button>
 		</div>
